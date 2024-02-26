2735612db0b8e0076054095bdaa8fe02ad621f8a
Conor Timmis <mctto@protonmail.com>
2024-02-26 14:40:33 +0000

    Define Survey Greeting & main Functions

--- run.py
+++ run.py
@@ -11,3 +11,12 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('social_insights')
+
+def greeting():
+    print("Welcome to my Social Media Survey\n" +
+          "Please take your time and feel free to answer my questions below: ")
+
+def main():
+    greeting()
+
+main()
