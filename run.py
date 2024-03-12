f751d6a7d0547556e0996115c13c4f4fdb00c68d
Conor Timmis <mctto@protonmail.com>
2024-03-12 18:55:31 +0000

    Fix all errors found by pep8

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import gspread
 from google.oauth2.service_account import Credentials
 import os
 
+
 def clear():
     """
     Clear function to clean-up the terminal so things don't get messy.
     """
     os.system("cls" if os.name == "nt" else "clear")
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,21 +23,31 @@ SURVEY_RESULTS_SHEET = GSPREAD_CLIENT.open('social_insights')
 NAME = None
 AGE = 0
 
+
 def greeting():
     """
     Greets the user upon starting the Survey
     """
-    print("Welcome to my Social Media Survey\n" +
-          "Please take your time and feel free to answer my questions below: \n")
+    print(
+        "Welcome to my Social Media Survey\n"
+        "Please take your time, \n"
+        "and feel free to answer my questions below: \n"
+    )
 
 
 def name():
     """
-    Keep a user inside of a while-loop until they give a valid name using alpha.
+    Keep a user inside of a while-loop until
+    they give a valid name using alpha.
     """
     while True:
-        prompt = "Please enter your name below, it can be full name or just your first name: "
+        prompt = (
+            "Please enter your name below, \n"
+            "it can be full name or "
+            "just your first name: \n"
+        )
         NAME = input(prompt).strip().title()
+        clear()
         if NAME.isalpha():
             return NAME
         else:
@@ -44,38 +56,49 @@ def name():
 
 def age(sv_name):
     """
-    Keep a user inside of a while-loop until they give a valid age using numbers.
+    Keep a user inside of a while-loop
+    until they give a valid age using numbers.
     """
     while True:
         AGE = input(f"Welcome {sv_name}, how old are you?\n")
-        if AGE.isdigit(): 
-            return int(AGE)  
+        if AGE.isdigit():
+            return int(AGE)
         else:
             print(f"Sorry, {AGE} is invalid, please use only numbers.")
 
 
 def screen_time(sv_name, sv_age, worksheet):
     """
-    Keep a user inside a while-loop that also checks all characters for number & decimal input.
+    Keep a user inside a while-loop that also
+    checks all characters for number & decimal input.
     """
-    print(f"Great! Your name is {sv_name}, and you are {sv_age}.\n" +
-          "Please share your average screen time daily in hours, this can also include decimals for specificity: ")
+    print(
+        f"Great! Your name is {sv_name}, and you are {sv_age}.\n"
+        "Please share your average screen time daily in hours, "
+        "this can also include decimals for specificity:\n"
+    )
 
     while True:
         sv_time = input()
+        clear()
         if all(char.isdigit() or char == '.' for char in sv_time):
             return sv_time
         else:
-            print("Sorry, please enter your screen time using only numbers and decimal points.")
+            print(
+                "Sorry, please enter your screen time, \n"
+                "using only numbers and decimal points."
+            )
 
 
 def sv_yesno(prompt):
     """
-    Prompts the user during the survey to answer only with "Yes" or No" if called.
+    Prompts the user during the survey to
+    answer only with "Yes" or No" if called.
     """
     while True:
-        response = input(prompt).strip().title()  
-        if response in ["Yes", "No"]:  
+        response = input(prompt).strip().title()
+        clear()
+        if response in ["Yes", "No"]:
             return response
         else:
             print("Please enter either 'Yes' or 'No'.")
@@ -88,50 +111,80 @@ def sv_scale(prompt, min_value, max_value):
             if min_value <= response <= max_value:
                 return response
             else:
-                print(f"Sorry, please enter a number between {min_value} and {max_value}.")
+                print(
+                    f"Sorry, please enter a number "
+                    "between {min_value} and {max_value}."
+                    )
         except ValueError:
             print("Please enter a valid number.")
-    
+
 
 def sv_question1():
-    return sv_yesno("Do you feel like you are spending too much time on Social Media daily? (Yes/No) \n")
+    return sv_yesno(
+        "Do you feel like you are spending too much time \n"
+        "on Social Media daily? (Yes/No) \n"
+    )
 
 
 def sv_question2():
-    return sv_yesno("Do you find yourself feeling the need to be on Social Media  often? (Yes/No) \n")
+    return sv_yesno(
+        "Do you find yourself feeling the need \n"
+        "to be on Social Media often? (Yes/No) \n"
+    )
 
 
 def sv_question3():
-    return sv_scale("On a scale of 1 to 10, how frequently do you check for notifications? (1 being rarely, 10 being always): \n", 1, 10)
+    return sv_scale(
+        "On a scale of 1 to 10, how frequently \n"
+        "do you check for notifications? \n"
+        "(1 being rarely, 10 being always): \n",
+        1, 10
+    )
 
 
 def sv_question4():
-    return sv_scale("On a scale of 1 to 10, how often do you ignore other important activities or responsibilities because of social media use? (1 being rarely, 10 being always): \n", 1, 10)
+    return sv_scale(
+        "On a scale of 1 to 10, how often do you ignore other important \n"
+        "activities or responsibilities because of social media use? \n"
+        "(1 being rarely, 10 being always): \n",
+        1, 10
+    )
 
 
 def sv_question5():
     while True:
-        response = input("How often do you find yourself comparing your life to others' on social media?\n"
-                         "A. Frequently, I often feel like a failure in comparison\n"
-                         "B. Occasionally, but I try not to let it affect me\n"
-                         "C. Rarely, I know that people usually show only the best parts of their lives on social media\n"
-                         "Please choose A, B, or C: \n").strip().upper()
+        response = input(
+            "How often do you find yourself comparing \n"
+            "your life to others' on social media?\n"
+            "A. Frequently, I often feel like a failure in comparison\n"
+            "B. Occasionally, but I try not to let it affect me\n"
+            "C. Rarely, I know that people usually show only the\n"
+            "best parts of their lives on social media\n"
+            "Please choose A, B, or C: \n").strip().upper()
+        clear()
         if response in ['A', 'B', 'C']:
             return response
         else:
             print("Please select a valid option (A, B, or C).")
 
+
 def sv_question6():
     while True:
-        response = input("How do you feel when you see others having fun on social media while you're not included?\n"
-                         "A. Fear of missing out: I feel left out and envious\n"
-                         "B. Indifferent: I understand that not every moment is captured online\n"
-                         "C. Happy for them: I appreciate their experiences without feeling left out\n"
-                         "Choose A, B, or C: ").strip().upper()
+        response = input(
+            "How do you feel when you see others having fun "
+            "on social media while you're not included?\n"
+            "A. Fear of missing out: I feel left out and envious\n"
+            "B. Indifferent: I understand that not "
+            "every moment is captured online\n"
+            "C. Happy for them: I appreciate their experiences \n"
+            "without feeling left out\n"
+            "Choose A, B, or C: ").strip().upper()
+        clear()
         if response in ['A', 'B', 'C']:
             return response
         else:
             print("Please select a valid option (A, B, or C).")
+
 
 def sv_analysis(responses):
     """
@@ -142,15 +195,31 @@ def sv_analysis(responses):
     for response in responses:
         if response in ['Yes', 10, 'A']:
             negative_count += 1
-        elif response in ['No', 'B', 'C'] or (isinstance(response, int) and response < 5):
+        elif response in ['No', 'B', 'C'] or (isinstance(response, int) and response < 5):  # NOQA
             positive_count += 1
 
+    clear()
     if positive_count > negative_count:
-        print("You seem perfectly well with the way you're going, a completely healthy user of social media.")
+        print(
+            "You seem perfectly well with the way you're going, \n"
+            "a completely healthy user of social media."
+        )
     elif positive_count < negative_count:
-        print("In my personal experience, I think you should look into self-improvement for time spent on your social media apps.")
+        print(
+            "From my personal experience, I think you should \n"
+            "look into self-improvement for time spent \n"
+            "on your social media apps."
+        )
     else:
-        print("Your answers overall are okay, your social media use seems balanced.")
+        print(
+            "Your answers overall are okay, "
+            "your social media use seems balanced."
+        )
+    print(
+        "\nThank you for taking the time to fill out my survey\n"
+        "Check back every month for a new survey."
+    )
+
 
 def sv_handle():
     """
@@ -171,10 +240,11 @@ def main():
     """
     Runs the survey program with all the functions declared.
     """
+    clear()
     greeting()
     sv_name = name()
     sv_age = age(sv_name)
-    
+
     worksheet = SURVEY_RESULTS_SHEET.sheet1
 
     sv_time = screen_time(sv_name, sv_age, worksheet)
@@ -184,9 +254,19 @@ def main():
     svy_question4 = sv_question4()
     svy_question5 = sv_question5()
     svy_question6 = sv_question6()
-    
-    worksheet.append_row([sv_name, sv_age, sv_time, svy_question1, svy_question2, svy_question3, svy_question4, svy_question5, svy_question6])
 
-    sv_analysis([svy_question1, svy_question2, svy_question3, svy_question4, svy_question5, svy_question6])
+    worksheet.append_row([
+        sv_name, sv_age, sv_time,
+        svy_question1, svy_question2,
+        svy_question3, svy_question4,
+        svy_question5, svy_question6
+    ])
+
+    sv_analysis([
+        svy_question1, svy_question2,
+        svy_question3, svy_question4,
+        svy_question5, svy_question6
+    ])
+
 
 main()
